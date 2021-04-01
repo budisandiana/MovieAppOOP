3ba3ce9dd29e436871ba180ea1c7c9874dbbb691
budisandiana <61983236+budisandiana@users.noreply.github.com>
2021-04-01 22:59:02 +0300

    Add files via upload

---
+++ Filme.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+class Filme
+{
+private:
+	std::string title;
+	std::string genre;
+	int year; // appearance year
+	int like;
+	std::string trailer; // youtube link
+
+public:
+	// default constructor
+	Filme();
+
+	// constructor with parameters
+	Filme(const std::string &title, const std::string &genre, const int &year, const int &like, const std::string &trailer);
+
+	std::string getTitle() const { return title; }
+	std::string getGenre() const { return genre; }
+	int getYear() const { return year; }
+	int getLike() const { return like; }
+	std::string getTrailer() const { return trailer; }
+
+	bool operator==(const Filme& m);
+
+	// Plays the current song: the page corresponding to the source link is opened in a browser.
+	void play();
+
+	friend std::istream& operator>>(std::istream& is, Filme& m);
+	friend std::ostream& operator<<(std::ostream& os, const Filme& m);
+};
+
---
+++ Repository.cpp
@@ -0,0 +1,74 @@
+#include "Repository.h"
+#include <string>
+#include <fstream>
+#include "Utils.h"
+#include "RepositoryExceptions.h"
+
+using namespace std;
+
+Repository::Repository(const std::string& filename)
+{
+	this->filename = filename;
+	this->readFromFile();
+}
+
+void Repository::addMovie(const Filme& m)
+{
+	Filme m1{};
+	try
+	{
+		m1 = this->findByTitleandGenre(m.getTitle(), m.getGenre());
+		throw DuplicateMovieException();
+	}
+	catch (InexistentMovieException& e) {}
+	this->movielist.push_back(m);
+	this->writeToFile();
+}
+
+void Repository::removeMovie(const Filme& m)
+{
+	auto it = find(this->movielist.begin(), this->movielist.end(), m);
+	if (it == this->movielist.end())
+		throw InexistentMovieException{};
+	this->movielist.erase(it);
+	this->writeToFile();
+}
+
+Filme Repository::findByTitleandGenre(const std::string& title, const std::string& genre) const
+{
+	for (auto m : this->movielist)
+	{
+		if (m.getTitle() == title && m.getGenre() == genre)
+			return m;
+	}
+
+	throw InexistentMovieException{};
+}
+
+void Repository::readFromFile()
+{
+	ifstream file(this->filename);
+
+	if (!file.is_open())
+		throw FileException("The file could not be opened!");
+
+	Filme m;
+	while (file >> m)
+		this->movielist.push_back(m);
+
+	file.close();
+}
+
+void Repository::writeToFile()
+{
+	ofstream file(this->filename);
+	if (!file.is_open())
+		throw FileException("The file could not be opened!");
+
+	for (auto m : this->movielist)
+	{
+		file << m;
+	}
+
+	file.close();
+}
